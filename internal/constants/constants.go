eac738ef59e55ed3a9fe36133d57c9dd8e85d284
Maxim Bogdanov <lolerpro4@gmail.com>
2025-06-04 17:23:54 +0300

    [feet]: added a constants for logger

---
+++ internal/constants/constants.go
@@ -0,0 +1,17 @@
+package constants
+
+const (
+	// LogLevelDebug уровень логирования для отладки.
+	LogLevelDebug = "DEBUG"
+	// LogLevelInfo уровень логирования для информационных сообщений.
+	LogLevelInfo = "INFO"
+	// LogLevelWarn уровень логирования для предупреждений.
+	LogLevelWarn = "WARN"
+	// LogLevelError уровень логирования для ошибок.
+	LogLevelError = "ERROR"
+
+	// LogFormatJSON формат логов в JSON.
+	LogFormatJSON = "json"
+	// LogFormatText формат логов в текстовом виде.
+	LogFormatText = "text"
+)
