49e832ae5848603341f6eb1db16abee4a694ce5f
Maxim Bogdanov <lolerpro4@gmail.com>
2025-06-04 21:10:15 +0300

    [refactor]: The pointer was removed

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -9,7 +9,7 @@ import (
 type DefaultWorker struct{}
 
 // Run выполняет цикл обработки задач
-func (w *DefaultWorker) Run(ctx context.Context, tasks <-chan interfaces.Task, id int32, log interfaces.Logger) {
+func (w DefaultWorker) Run(ctx context.Context, tasks <-chan interfaces.Task, id int32, log interfaces.Logger) {
 	log.Debug("Воркер запущен", zap.Int32("worker_id", id))
 	for {
 		select {
